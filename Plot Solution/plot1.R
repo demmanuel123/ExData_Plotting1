987fc0b8e38eed9d3b007265708d499ef0dc050d
denure <daina.emmanuel@aol.com>
2015-02-07 15:04:00 +0530

    Solution for Plot 1

---
+++ Plot Solution/plot1.R
@@ -0,0 +1,21 @@
+library(rJava)
+library(data.table)
+# Pre requisitive : Set working directory where you will find the input data in text format
+# using setwd("<directory name>")
+household_power <- fread("household_power_consumption.txt", sep=";", header=TRUE,colClasses="character")
+#Subset and extract only 1/2 and 2/2 dates specific data 
+
+powernew <-subset(household_power, household_power$Date == "1/2/2007" | household_power$Date =="2/2/2007" )
+
+# Initialize to PNG file 
+png("plot1.png",width=6,height=6,units="in",res=1200)
+
+# Histogram with X axis as Global active power
+# col as red color
+
+hist(as.numeric(powernew$Global_active_power)
+     ,col="red"
+     ,main="Global active power"
+     ,xlab="Global active power(kilowatts)"
+     ,)
+dev.off() #device off
